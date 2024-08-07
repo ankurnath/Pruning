590b4e7cb6334b94051ad6b3db231b66728c4b8d
Ankur Nath <anath@csce-64q58y3.engr.tamu.edu>
2024-08-07 12:14:59 -0500

    new code

---
+++ Data Preprocessing/utils.py
@@ -0,0 +1,28 @@
+import pickle
+def load_from_pickle(file_path):
+    """
+    Load data from a pickle file.
+
+    Parameters:
+    - file_path: The path to the pickle file.
+
+    Returns:
+    - loaded_data: The loaded data.
+    """
+    with open(file_path, 'rb') as file:
+        loaded_data = pickle.load(file)
+    print(f'Data has been loaded from {file_path}')
+    return loaded_data
+
+
+def save_to_pickle(data, file_path):
+    """
+    Save data to a pickle file.
+
+    Parameters:
+    - data: The data to be saved.
+    - file_path: The path to the pickle file.
+    """
+    with open(file_path, 'wb') as file:
+        pickle.dump(data, file)
+    print(f'Data has been saved to {file_path}')
